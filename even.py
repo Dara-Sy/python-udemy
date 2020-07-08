2c4af8a3441c9ecf8339588e943c11f5c108f92b
Dara-Sy <dara.sy@intersection.com>
2020-07-08 18:09:24 -0400

    print range in 50

---
+++ even.py
@@ -0,0 +1,19 @@
+#Define a function called generate_evens
+#It should return a list of even numbers between 1 and 50(not including 50)
+
+# even_list = []
+def generate_evens():    
+    a=[]
+    for i in range(50):    
+        a.append(i)
+
+    print(a)
+
+
+generate_evens()
+
+# a=[]
+# for i in range(5):    
+#     a.append(i)
+
+# print(a)
